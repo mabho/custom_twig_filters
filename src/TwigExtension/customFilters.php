8af96decd9865a3bbbb9bbb199417bb4e22acf26
Marcos Buarque de Hollanda <marcos.buarque@uol.com.br>
2021-05-26 10:15:16 -0300

    Minor positioning change on opening brace.

--- src/TwigExtension/customFilters.php
+++ src/TwigExtension/customFilters.php
@@ -6,16 +6,15 @@
  */
 namespace Drupal\custom_twig_filters\TwigExtension;
 class customFilters extends \Twig_Extension {
-  public function getFilters()
-  {
+  public function getFilters() {
       return [ new \Twig_SimpleFilter('slugify', array($this, 'slugify'))];
   }
-  public function getName()
-  {
+
+  public function getName() {
     return 'custom_twig_filters.twig_extension';
   }
-  public static function slugify($string)
-  {
+
+  public static function slugify($string) {
     // Trim, remove special characters and strip HTML tags.
     $string = html_entity_decode(trim(strip_tags($string)));
 
