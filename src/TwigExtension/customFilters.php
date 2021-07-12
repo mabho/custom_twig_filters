c59602660d7ee01f0ff6808c54a42aace1e71b94
Marcos Buarque de Hollanda <marcos.buarque@uol.com.br>
2021-07-12 09:36:37 -0300

    Incorporated a new TWIG filter: slugify_from_tag.

--- src/TwigExtension/customFilters.php
+++ src/TwigExtension/customFilters.php
@@ -1,19 +1,28 @@
 <?php
+
 /**
  * Created by Marcos Hollanda.
  * Date: 9/28/2019
  * Time: 02:56 PM
  */
+
 namespace Drupal\custom_twig_filters\TwigExtension;
-class customFilters extends \Twig_Extension {
+
+class customFilters extends \Twig_Extension
+{
   public function getFilters()
   {
-      return [ new \Twig_SimpleFilter('slugify', array($this, 'slugify'))];
+    return [
+      new \Twig_SimpleFilter('slugify_from_tag', array($this, 'slugify_from_tag')),
+      new \Twig_SimpleFilter('slugify', array($this, 'slugify')),
+    ];
   }
+
   public function getName()
   {
     return 'custom_twig_filters.twig_extension';
   }
+
   public static function slugify($string)
   {
     // Trim, remove special characters and strip HTML tags.
@@ -24,5 +33,27 @@ class customFilters extends \Twig_Extension {
 
     // Delivers a slug.
     return $clean_string;
+  }
+
+  public static function slugify_from_tag($string, $tag = 'h3')
+  {
+
+    // a new dom object 
+    $dom = new \domDocument('1.0', 'utf-8');
+
+    // load the html into the object 
+    $dom->loadHTML($string);
+
+    // Discard white space 
+    $dom->preserveWhiteSpace = FALSE;
+
+    // Strips undesired characters from tag.
+
+    $strippedtag = $tag; // preg_replace('\<\>', '\\1', $h1);
+
+    $strippedtext_group = $dom->getElementsByTagName($strippedtag); // here u use your desired tag
+    $strippedtext = $strippedtext_group->item(0)->nodeValue;
+
+    return \Drupal::service('pathauto.alias_cleaner')->cleanString($strippedtext);
   }
 }
